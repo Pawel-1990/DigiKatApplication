628e546cde1069c8f742389b0b4c73ab6ddc0d4a
Pawel Dyjak <pawel.dyjak@poczta.onet.pl>
2025-07-30 19:08:58 +0200

    Application created

---
+++ src/test/java/pl/digikat/digikatapplication/DigiKatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.digikat.digikatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiKatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
